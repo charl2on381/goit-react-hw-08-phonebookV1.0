1bcc624f4fd2817efdc87117c306e3c410f167e0
Charlton Teo <41657722+charl2on381@users.noreply.github.com>
2024-03-30 22:37:02 +0800

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,9 @@
+import css from './Footer.module.css';
+
 export const Footer = () => {
   return (
-    <footer>
-      &copy; {new Date().getFullYear()} goit-react-hw-08-phonebook
+    <footer className={css.container}>
+      &copy; {new Date().getFullYear()} By: Charlton Teo | goit-react-hw-08-phonebook
     </footer>
   );
 };
